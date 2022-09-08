d6f74695d56650b83f84d489faaf719370b68b44
nicholusmwenda <nndegwa3@gmail.com>
2022-09-08 13:33:00 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- *
- * *main-entry point
- *
- * *
- *
- * *return always 0 (success)
- *
- * **/
-
-
+*main-entry point
+*
+*return always 0 (success)
+**/
 
 int main()
-
 {
-
-		puts("Programming is like building a multilingual puzzle\n");
-
-			return 0;
-
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
