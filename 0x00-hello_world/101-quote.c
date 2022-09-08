b6accf9a946395338c336270f5297a6fc8aeba3b
nicholusmwenda <nndegwa3@gmail.com>
2022-09-08 14:20:37 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 /**
  *  main - prints exactly "and that piece of art is useful"
+ *
+ *  Description: and that piece of art is useful" - Dora Korpar, 2015-10-19
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
  */
