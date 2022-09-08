6a734c3a0099721af2cca1df9d2cd964bde5bf94
nicholusmwenda <nndegwa3@gmail.com>
2022-09-08 13:13:50 -0700

    0x03-shell_variables_expansions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+
+
+/**
+ *
+ * *main-entry point
+ *
+ * *
+ *
+ * *return always 0 (success)
+ *
+ * **/
+
+
+
+int main()
+
+{
+
+		puts("Programming is like building a multilingual puzzle\n");
+
+			return 0;
+
+}
