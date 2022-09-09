9af68e30a43c8c2e4301a5011bf60b96d046059e
nicholusmwenda <nndegwa3@gmail.com>
2022-09-09 13:43:19 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all the single numbers
+ * followed by a new line
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
